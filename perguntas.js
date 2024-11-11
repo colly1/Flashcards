f437f8a7fb5300c85c994ecdba11836b535df54b
colly1 <165697868+colly1@users.noreply.github.com>
2024-11-11 14:14:35 -0300

    bloco

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação',
+    'O que ê JavaScript',
+    'O JavaScript ê uma linguagem de programação',
+
+)
+criaCartao(
+    'Geografia',
+    'Qual ê a capital da frança?',
+    'A capital da frança ê Paris ',
+)
+
+criaCartao(
+    'programação',
+    'O que ê uma funçaõ?',
+    'Uma função ê um bloco de código que executa alguma tarefa'
+
+)
